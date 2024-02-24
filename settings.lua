178c6e16285d9e8bb981838562d343df4fd68f9e
Biggest-Chad <159510415+Biggest-Chad@users.noreply.github.com>
2024-02-24 15:51:06 +0700

    updated settings.lua

--- settings.lua
+++ settings.lua
@@ -1,6 +1,6 @@
 local _, t = ...
-t.my_name = "Golemagg Discord Blocklist"
-t.my_provider = "https://discord.gg/golemagg"
-t.my_realm = "Golemagg"
-t.my_description = "A list of scammers curated by the golemagg discord community."
-t.my_url = "https://discord.gg/golemagg"
+t.my_name = "Shadowstrike Discord Blacklist"
+t.my_provider = "https://discord.gg/8m9ZzvwZKk"
+t.my_realm = "ShadowstrikeAU"
+t.my_description = "A list of evidenced bad actors (ninjas, trolls, toxic players) curated by the Shadowstrike AU discord community."
+t.my_url = "https://discord.gg/8m9ZzvwZKk"
